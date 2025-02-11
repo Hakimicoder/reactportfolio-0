9a75483ff68498f9622879ec75379a9da8d3659a
Hakimicoder <git@stackblitz.com>
2025-02-11 10:46:13 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div>
+
+        <div className="header">
+          <h2 className="logo">Jumy</h2>
+          <nav className="navbar">
+            <a href="#">Acceuil</a>
+            <a href="#">A propos</a>
+            <a href="#">services</a>
+            <a href="#">contact</a>
+            <a href="#" className="btn">devis gratuit</a>
+          </nav>
+        </div>
+
+        <div className="banner">
+          <h2></h2>
+        </div>
+
+        </div>
+    </>
+  )
+}
+
+export default App
